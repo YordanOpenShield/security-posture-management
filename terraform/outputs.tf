f5d9b06f6ebc89ef5361e4f6d47707d0a1ee2674
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-07 11:13:16 +0300

    Set kubeconfig output as sensitive to enhance security

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,7 @@
 output "kubeconfig" {
   description = "Kubeconfig to access the created cluster (base64 encoded)"
   value       = digitalocean_kubernetes_cluster.spm_cluster.kube_config
+  sensitive = true
 }
 
 output "cluster_name" {
