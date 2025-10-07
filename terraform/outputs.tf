75f626f7a3902682630c3876531d045371e37287
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-07 11:11:01 +0300

    Fix kubeconfig output value to reference the correct attribute for DigitalOcean cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "kubeconfig" {
   description = "Kubeconfig to access the created cluster (base64 encoded)"
-  value       = digitalocean_kubernetes_cluster.spm_cluster.kube_config[0].raw_kube_config
+  value       = digitalocean_kubernetes_cluster.spm_cluster.kube_config
 }
 
 output "cluster_name" {
