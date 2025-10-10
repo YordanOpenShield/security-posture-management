17348aa939fe7cef4f522b3903ac8d4d00f4406c
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-10 18:34:09 +0300

    Implement code changes to enhance functionality and improve performance

--- terraform/modules/tenant/faraday_redis.tf
+++ terraform/modules/tenant/faraday_redis.tf
@@ -1,10 +1,10 @@
 resource "kubernetes_service" "redis" {
   metadata {
-    name      = "redis"
+    name      = "faraday-redis"
     namespace = kubernetes_namespace.tenant_ns.metadata[0].name
   }
   spec {
-    selector = { app = "redis" }
+    selector = { app = "faraday-redis" }
     port {
       port        = 6379
       target_port = 6379
@@ -16,25 +16,25 @@ resource "kubernetes_service" "redis" {
 
 resource "kubernetes_deployment" "redis" {
   metadata {
-    name      = "redis"
+    name      = "faraday-redis"
     namespace = kubernetes_namespace.tenant_ns.metadata[0].name
-    labels = { app = "redis" }
+    labels = { app = "faraday-redis" }
   }
 
   spec {
     replicas = 1
     selector {
-      match_labels = { app = "redis" }
+      match_labels = { app = "faraday-redis" }
     }
 
     template {
       metadata {
-        labels = { app = "redis" }
+        labels = { app = "faraday-redis" }
       }
 
       spec {
         container {
-          name  = "redis"
+          name  = "faraday-redis"
           image = var.redis_image
           image_pull_policy = "IfNotPresent"
           port {
