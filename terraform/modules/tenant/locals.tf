7298b3c1db784eb63472be2fc8dd6747da02e278
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-13 11:53:55 +0300

    fix(terraform): simplify chosen_server_type assignment to use the first element directly

--- terraform/modules/tenant/locals.tf
+++ terraform/modules/tenant/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  chosen_server_type = values(data.hcloud_server_type.selected)[keys(data.hcloud_server_type.selected)[0]].name
+  chosen_server_type = values(data.hcloud_server_type.selected)[0].name
 }
