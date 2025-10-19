e7750fa3696b5dff8c655df596cffc89593f4f84
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-19 15:14:29 +0300

    fix(terraform): update Faraday installation script to remove non-interactive frontend export

--- terraform/modules/tenant/faraday.tf
+++ terraform/modules/tenant/faraday.tf
@@ -26,53 +26,56 @@ resource "local_file" "configure_faraday_nginx_sh" {
 }
 
 resource "null_resource" "provision_faraday_scripts" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "file" {
-    source      = local.render_dir
-    destination = "/tmp/scripts"
-
-
-    connection {
-      type        = "ssh"
-      host        = hcloud_server.tenant_server.ipv4_address
-      port        = 2222
-      user        = var.provision_user
-      private_key = tls_private_key.ssh_key.private_key_pem
-
-      agent       = false
+    triggers = {
+        always_run = timestamp()
     }
-  }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update -y",
-      # Make sure postgres and redis are running before installing Faraday
-      "while ! sudo systemctl is-active --quiet postgresql; do echo 'Waiting for PostgreSQL to start...'; sleep 5; done",
-      "while ! sudo systemctl is-active --quiet redis; do echo 'Waiting for Redis to start...'; sleep 5; done",
-      # Execute installation and configuration scripts
-      "sudo chmod +x /tmp/scripts/install-faraday.sh",
-      "sudo chmod +x /tmp/scripts/install-nginx.sh",
-      "sudo chmod +x /tmp/scripts/configure-faraday-nginx.sh",
-      "sudo /tmp/scripts/install-faraday.sh",
-      "sudo /tmp/scripts/install-nginx.sh",
-      "sudo /tmp/scripts/configure-faraday-nginx.sh",
-      # Cleanup
-      "rm -rf /tmp/scripts"
-    ]
+    provisioner "file" {
+        source      = local.render_dir
+        destination = "/tmp/scripts"
 
-    connection {
-      type        = "ssh"
-      host        = hcloud_server.tenant_server.ipv4_address
-      port        = 2222
-      user        = var.provision_user
-      private_key = tls_private_key.ssh_key.private_key_pem
-      agent       = false
-      timeout     = "10m"
+
+        connection {
+        type        = "ssh"
+        host        = hcloud_server.tenant_server.ipv4_address
+        port        = 2222
+        user        = var.provision_user
+        private_key = tls_private_key.ssh_key.private_key_pem
+
+        agent       = false
+        }
     }
-  }
+
+    provisioner "remote-exec" {
+        inline = [
+        # Set non-interactive frontend for apt
+        "export DEBIAN_FRONTEND=noninteractive",
+        # Update package lists
+        "sudo apt update -y",
+        # Make sure postgres and redis are running before installing Faraday
+        "while ! sudo systemctl is-active --quiet postgresql; do echo 'Waiting for PostgreSQL to start...'; sleep 5; done",
+        "while ! sudo systemctl is-active --quiet redis; do echo 'Waiting for Redis to start...'; sleep 5; done",
+        # Execute installation and configuration scripts
+        "sudo chmod +x /tmp/scripts/install-faraday.sh",
+        "sudo chmod +x /tmp/scripts/install-nginx.sh",
+        "sudo chmod +x /tmp/scripts/configure-faraday-nginx.sh",
+        "sudo /tmp/scripts/install-faraday.sh",
+        "sudo /tmp/scripts/install-nginx.sh",
+        "sudo /tmp/scripts/configure-faraday-nginx.sh",
+        # Cleanup
+        "rm -rf /tmp/scripts"
+        ]
+
+        connection {
+        type        = "ssh"
+        host        = hcloud_server.tenant_server.ipv4_address
+        port        = 2222
+        user        = var.provision_user
+        private_key = tls_private_key.ssh_key.private_key_pem
+        agent       = false
+        timeout     = "10m"
+        }
+    }
 
     depends_on = [
         cloudflare_dns_record.faraday,
