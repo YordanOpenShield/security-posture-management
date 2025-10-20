5fffbb5bdcf20cdbdbb3c4ebff006814cd9d4751
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-20 16:53:04 +0300

    fix(terraform): update output names in tenant module for consistency

--- terraform/modules/tenant/output.tf
+++ terraform/modules/tenant/output.tf
@@ -1,17 +1,17 @@
-output "tenant_host" {
+output "host" {
     value = hcloud_server.tenant_server.ipv4_address
 }
 
-output "tenant_ssh_private_key" {
+output "ssh_private_key" {
   value     = tls_private_key.ssh_key.private_key_pem
   sensitive = true
 }
 
-output "tenant_ssh_public_key" {
+output "ssh_public_key" {
   value = tls_private_key.ssh_key.public_key_openssh
 }
 
-output "tenant_faraday_credentials" {
+output "faraday_credentials" {
   value = {
     url      = "https://faraday.${var.name}.${var.spm_subdomain}.${var.base_domain}"
     username = "admin"
--- terraform/tenant.tf
+++ terraform/tenant.tf
@@ -12,14 +12,19 @@ output "tenant_name" {
 }
 
 output "tenant_host" {
-  value = module.tenant.tenant_host
+  value = module.tenant.host
 }
 
 output "tenant_ssh_private_key" {
-  value     = module.tenant.tenant_ssh_private_key
+  value     = module.tenant.ssh_private_key
   sensitive = true
 }
 
 output "tenant_ssh_public_key" {
-  value = module.tenant.tenant_ssh_public_key
+  value = module.tenant.ssh_public_key
+}
+
+output "tenant_faraday_credentials" {
+  value     = module.tenant.faraday_credentials
+  sensitive = true
 }
