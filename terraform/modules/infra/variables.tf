46ab4ea2e7238ebd4da9a32859a8fe9af38bc89d
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-21 13:54:32 +0300

    feat(terraform): add server CPU and RAM variables for configuration

--- terraform/modules/infra/variables.tf
+++ terraform/modules/infra/variables.tf
@@ -15,6 +15,18 @@ variable "server_image" {
   default     = "ubuntu-22.04"
 }
 
+variable "server_cpu" {
+  description = "The minimum number of CPU cores for the server"
+  type        = number
+  default     = 2
+}
+
+variable "server_ram" {
+  description = "The minimum number of GBs of RAM for the server"
+  type        = number
+  default     = 4
+}
+
 variable "volume_size" {
   description = "Size of the volume in GB"
   type        = number
