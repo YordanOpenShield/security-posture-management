2e472d786d56c7afea7b257ad42360e9195e6b1e
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-21 15:32:46 +0300

    feat(terraform): reintroduce tenant module and outputs in tenant.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,7 @@
-locals {
-    base_domain = "openshield.io"
-    spm_subdomain = "spm"
-    faraday_subdomain = "faraday"
+# locals {
+#     base_domain = "openshield.io"
+#     spm_subdomain = "spm"
+#     faraday_subdomain = "faraday"
 
-    provision_user = "deploy"
-}
+#     provision_user = "deploy"
+# }
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,21 +1,21 @@
-output "tenant_name" {
-  value = var.tenant
-}
+# output "tenant_name" {
+#   value = var.tenant
+# }
 
-output "tenant_host" {
-  value = module.infrastructure.host
-}
+# output "tenant_host" {
+#   value = module.infrastructure.host
+# }
 
-output "tenant_ssh_private_key" {
-  value     = module.infrastructure.ssh_private_key
-  sensitive = true
-}
+# output "tenant_ssh_private_key" {
+#   value     = module.infrastructure.ssh_private_key
+#   sensitive = true
+# }
 
-output "tenant_ssh_public_key" {
-  value = module.infrastructure.ssh_public_key
-}
+# output "tenant_ssh_public_key" {
+#   value = module.infrastructure.ssh_public_key
+# }
 
-output "tenant_faraday_credentials" {
-  value     = module.faraday.credentials
-  sensitive = true
-}
+# output "tenant_faraday_credentials" {
+#   value     = module.faraday.credentials
+#   sensitive = true
+# }
