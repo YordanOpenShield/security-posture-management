f93d1600b64fd4c088eedfbdc5e330c7bc9acf5b
YordanOpenShield <yordan.georgiev1711@gmail.com>
2025-10-21 15:48:00 +0300

    feat(terraform): uncomment and enable locals and output definitions in Terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,38 @@
-# # ===============================================
-# # Infrastructure Module
-# # ===============================================
+# ===============================================
+# Infrastructure Module
+# ===============================================
 
-# module "infrastructure" {
-#     source = "./modules/infrastructure"
+module "infrastructure" {
+    source = "./modules/infrastructure"
 
-#     hetzner_token    = var.hetzner_token
-#     tenant_name      = var.tenant
-#     provision_user   = local.provision_user
-# }
+    hetzner_token    = var.hetzner_token
+    tenant_name      = var.tenant
+    provision_user   = local.provision_user
+}
 
-# # ===============================================
-# # DNS Module
-# # ===============================================
+# ===============================================
+# DNS Module
+# ===============================================
 
-# module "dns" {
-#     source = "./modules/dns"
+module "dns" {
+    source = "./modules/dns"
 
-#     cloudflare_token = var.cloudflare_token
-#     tenant_name      = var.tenant
-#     tenant_host      = module.infrastructure.host
-#     faraday_host     = "${local.faraday_subdomain}.${var.tenant}.${local.spm_subdomain}.${local.base_domain}"
-#     base_domain      = local.base_domain
-# }
+    cloudflare_token = var.cloudflare_token
+    tenant_name      = var.tenant
+    tenant_host      = module.infrastructure.host
+    faraday_host     = "${local.faraday_subdomain}.${var.tenant}.${local.spm_subdomain}.${local.base_domain}"
+    base_domain      = local.base_domain
+}
 
-# # ===============================================
-# # Faraday Module
-# # ===============================================
+# ===============================================
+# Faraday Module
+# ===============================================
 
-# module "faraday" {
-#     source = "./modules/faraday"
+module "faraday" {
+    source = "./modules/faraday"
 
-#     hetzner_token     = var.hetzner_token
-#     provision_user    = local.provision_user
-#     provision_private_key = module.infrastructure.ssh_private_key
-#     faraday_host      = module.dns.faraday_dns_record
-# }
+    hetzner_token     = var.hetzner_token
+    provision_user    = local.provision_user
+    provision_private_key = module.infrastructure.ssh_private_key
+    faraday_host      = module.dns.faraday_dns_record
+}
